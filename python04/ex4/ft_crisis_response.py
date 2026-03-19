8ee603edb842ad70574ac015cb476b6f1ddf06bf
Bastian Granier <bgranier@c1r3p4.42nice.fr>
2026-03-19 17:17:19 +0100

    bal

--- python04/ex4/ft_crisis_response.py
+++ python04/ex4/ft_crisis_response.py
@@ -1,16 +1,13 @@
 def crisis_handler(filename):
     try:
-        # Détection du type d'accès
         if filename == "standard_archive.txt":
             print(f"ROUTINE ACCESS: Attempting access to '{filename}'...")
         else:
             print(f"CRISIS ALERT: Attempting access to '{filename}'...")
 
-        # Tentative d'ouverture sécurisée
         with open(filename, "r") as f:
             content = f.read()
 
-        # Succès
         print(f"SUCCESS: Archive recovered - ``{content.strip()}''")
         print("STATUS: Normal operations resumed")
 
@@ -30,10 +27,9 @@ def crisis_handler(filename):
 def main() -> None:
     print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===")
 
-    # Crises simulées
-    crisis_handler("lost_archive.txt")          # n'existe pas
-    crisis_handler("classified_vault.txt")      # permissions interdites
-    crisis_handler("standard_archive.txt")      # fichier normal
+    crisis_handler("lost_archive.txt")
+    crisis_handler("classified_vault.txt")
+    crisis_handler("standard_archive.txt")
 
     print("All crisis scenarios handled successfully. Archives secure.")
 
--- python07/ex1/SpellCard.py
+++ python07/ex1/SpellCard.py
@@ -3,3 +3,7 @@ from ex0.Card import Card
 class Spellcard():
 	def __init__(self, name: str, cost: int, rarity: str, effect_type: str):
 		
+	
+	def play(self, game_state: dict) -> dict:
+
+	def resolve_effect(self, targets: list) -> dict:
