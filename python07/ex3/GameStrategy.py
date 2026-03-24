0a9a96b921b2a61732bc4d60ac1fe1ba2d9e0890
Bastian Granier <bgranier@c1r3p6.42nice.fr>
2026-03-24 16:55:04 +0100

    debut 3

--- python07/ex3/GameStrategy.py
+++ python07/ex3/GameStrategy.py
@@ -3,11 +3,12 @@ from ex0.Card import Card
 from ex1.SpellCard import SpellCard
 
 class GameStrategy(ABC):
-	def __init__(self):
-		pass
+	def __init__(self, strategy: str):
+		self.strategy = strategy
 
 	@abstractmethod
 	def execute_turn(self, hand: list, battlefield: list) -> dict:
+		pass
 
 	@abstractmethod
 	def get_strategy_name(self) -> str:
